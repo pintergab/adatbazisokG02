7e57f2eeb8a204f306ff097523408ad6a5f3e454
pintergab <gabriella.pinter@stud.uni-corvinus.hu>
2025-04-25 17:46:57 +0200

    Add files via upload

---
+++ Házi2_adatmaszkolás.sql
@@ -0,0 +1,20 @@
+CREATE Table Ugyfel_Masked
+( LOGIN NVARCHAR(255) not null PRIMARY KEY,
+ EMAIL nvarchar(255) MASKED WITH (function='email()'),
+ NEV nvarchar(255) ,
+ SZULEV int MASKED WITH(function='random(1,5)'),
+ NEM nvarchar(1),
+ CIM nvarchar(255) MASKED WITH (function='partial(1, "XXX",2)') 
+)
+
+INSERT INTO Ugyfel_Masked
+(LOGIN, EMAIL, NEV, SZULEV, NEM, CIM)
+SELECt login, email, nev, szulev, nem, cim
+FROM ugyfel
+
+CREAte USER Felhasználó WITHOUT Login;
+GRANT SELECT ON Ugyfel_Masked TO Felhasználó
+
+EXECUTE AS User= 'Felhasználó';
+SELECT * FROM Ugyfel_Masked
+REVERT
